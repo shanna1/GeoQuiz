a3c344a51b10e579231e30ad4b893eb7eada29ce
Spencer <shanna@gustavus.edu>
2016-02-12 21:09:09 -0600

    update Challenge:From Button to ImageButton

--- app/src/main/java/com/bigneardranch/android/geoquiz/QuizActivity.java
+++ app/src/main/java/com/bigneardranch/android/geoquiz/QuizActivity.java
@@ -4,15 +4,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
 
-    private Button mPrevButton;
+    private ImageButton mPrevButton;
     private Button mTrueButton;
     private Button mFalseButton;
-    private Button mNextButton;
+    private ImageButton mNextButton;
     private TextView mQuestionTextView;
 
     private Question[] mQuestionBank = new Question[]{
@@ -74,7 +75,7 @@ public class QuizActivity extends AppCompatActivity {
             }
         });
 
-        mNextButton = (Button) findViewById(R.id.next_button);
+        mNextButton = (ImageButton) findViewById(R.id.next_button);
         mNextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -84,7 +85,7 @@ public class QuizActivity extends AppCompatActivity {
         });
         updateQuestion();
 
-        mPrevButton = (Button) findViewById(R.id.prev_button);
+        mPrevButton = (ImageButton) findViewById(R.id.prev_button);
         mPrevButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
